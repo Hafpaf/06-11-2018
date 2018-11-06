6a361ba7fb44e01ea94af45844718c5b1e11fe77
hafpaf <haf@hafnium.me>
2018-11-06 14:28:36 +0100

    init commit

---
+++ nand.py
@@ -0,0 +1,30 @@
+a = float(input("text", ))
+b = float(input("text", ))
+c = float(input("text", ))
+
+def polsk():
+    if b+c == 1:
+        return 1
+    if
+        return
+
+def polak():
+    if b == 1:
+        return 0
+    if b == 0:
+        return 1
+
+def vodka():
+    if (a + polak) == 1:
+        return 0
+    elif:
+         return 1
+
+def pv():
+    if vodka + polak == 1:
+        return 1
+    else:
+        return 0
+
+q = pv
+print(q)
