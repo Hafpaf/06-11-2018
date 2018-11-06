401be34b2cbf36b7564be30a08bf0460511f28d5
hafpaf <haf@hafnium.me>
2018-11-06 19:15:25 +0100

    add comments

--- nand.py
+++ nand.py
@@ -4,7 +4,7 @@ b = float(input("b: " , ))
 c = float(input("c: " , ))
 '''
 
-table = [[1,1,1],
+table = [[1,1,1], # table with all current posiblilites
         [1,1,0],
         [1,0,1],
         [0,1,1],
@@ -15,41 +15,41 @@ table = [[1,1,1],
 
 #print(tabel[2])
 
-for row in table:
+for row in table: #for loop that runs n amount of times until tables stop
     a = row[0]
     b = row[1]
     c = row[2]
 #    print(a,b,c)
 
-    def polsk(): #
+    def polsk(): #AND operator
         if b*c == 1:
             return 1
         else:
             return 0
 
-    def or_inv():
+    def or_inv(): #Inverse operator
         if polsk() == 0 and a == 0:
             return 1
         else:
             return 0
 
-    def polak():
+    def polak(): #NAND operator
         if b == 1:
             return 0
         else:
             return 1
 
     def vodka():
-        if (a + polak()) == 1:
+        if (a + polak()) == 1: #Inverse OR operator
             return 0
         else:
              return 1
 
-    def pv():
+    def pv(): #Inverse OR operator
         if vodka() and or_inv() == 0:
             return 1
         else:
             return 0
 
-    q = pv()
-    print(row,"=",q)
+    q = pv() #rename variable to q, as the assignment states
+    print(row,"=",q) #print which row are used and output
