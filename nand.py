c7cc0e6b9a8f89d02e33f59e6abeb55d73a619f0
hafpaf <haf@hafnium.me>
2018-11-06 15:30:56 +0100

    working table

--- nand.py
+++ nand.py
@@ -5,43 +5,44 @@ b = float(input("b: " , ))
 c = float(input("c: " , ))
 '''
 
-tabel = [[1,1,1],
-    [1,1,0],
-    [1,0,1],
-    [0,1,1],
-    [1,0,0],
-    [0,0,0]]
+table = [[1,1,1],
+        [1,1,0],
+        [1,0,1],
+        [0,1,1],
+        [1,0,0],
+        [0,0,0]]
 
-print(A)
+#print(tabel[2])
 
-for x in range(0,5):
-    a = []
-    tabel + [0]
+for row in table:
+    a = row[0]
+    b = row[1]
+    c = row[2]
+#    print(a,b,c)
 
-def polsk(): #
-    if b+c == 0 and a == 0:
-        return 1
-    else:
-        return 0
+    def polsk(): #
+        if b+c == 0 and a == 0:
+            return 1
+        else:
+            return 0
 
-def polak():
-    if b == 1:
-        return 0
-    if b == 0:
-        return 1
+    def polak():
+        if b == 1:
+            return 0
+        if b == 0:
+            return 1
 
-def vodka():
-    if (a + polak()) == 1:
-        return 0
-    else:
-         return 1
+    def vodka():
+        if (a + polak()) == 1:
+            return 0
+        else:
+             return 1
 
-def pv():
-    if vodka() + polak() == 1:
-        return 1
-    else:
-        return 0
+    def pv():
+        if vodka() + polak() == 1:
+            return 1
+        else:
+            return 0
 
-q = pv()
-print(q)
-'''
+    q = pv()
+    print(q)
