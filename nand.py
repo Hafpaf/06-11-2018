ceef1893077889fba8a2bf30a41c1eeb183eebbc
hafpaf <haf@hafnium.me>
2018-11-09 13:00:47 +0100

    add comment from Bombrabbit
    Co-Authored-By: Bombdevelops <Bombdevelops@users.noreply.github.com>

--- nand.py
+++ nand.py
@@ -32,30 +32,39 @@ for row in table: #for loop that runs n amount of times until tables stop
             return 1
         else:
             return 0
-
+#b*c
     def or_inv(): #Inverse operator
         if polsk() == 0 and a == 0:
             return 1
         else:
             return 0
 
+#inv(a+b)
     def polak(): #NAND operator
         if b == 1:
             return 0
         else:
             return 1
 
+#inv(b)
     def vodka():
         if (a + polak()) == 1: #Inverse OR operator
             return 0
         else:
              return 1
 
+#inv(inv(b)*a)
     def pv(): #Inverse OR operator
         if vodka() and or_inv() == 0:
             return 1
         else:
             return 0
-
     q = pv() #rename variable to q, as the assignment states
     print(row,"=",q) #print which row are used and output
+'''
+---------
+Svar a*inv(b) fordi kun 10 og 101 giver 1,
+Dette vil sige at c er ligegyldigt og ud fra det kan vi konkludere
+at det er a*inv(b) fordi så bliver der 11 ligmed 1.
+-----------
+'''
